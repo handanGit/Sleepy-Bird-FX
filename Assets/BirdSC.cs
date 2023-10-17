12766d58f6c6a67d18e7390831d9d1924aeb9d88
handanGit <handanmail18@gmail.com>
2023-10-17 09:06:31 +0700

    initial commit

---
+++ Assets/BirdSC.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BirdSC : MonoBehaviour
+{
+    public Rigidbody2D birdRigidBody;
+    public float flapStrenght;
+    public bool birdIsAlive = true;
+    public AudioSource flapSFX;
+    public AudioSource hitSFX;
+    //public GameObject birdShowcase;
+
+    public logicSC logic;
+    public movePipeSC pipe;
+    public GroundScript ground;
+    public pipeSpawnerSC pipeSpawn;
+    
+    [SerializeField] public float tiltingSpeed = 10f;
+    
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //to access logicSC
+        logic = GameObject.FindGameObjectWithTag("logic").GetComponent<logicSC>();
+        pipe = GameObject.FindGameObjectWithTag("pipe").GetComponent<movePipeSC>();
+        pipeSpawn = GameObject.FindGameObjectWithTag("pipeSpawner").GetComponent<pipeSpawnerSC>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //to make bird gain altitude
+        if (Input.GetMouseButtonDown(0) && birdIsAlive)
+        {
+            birdRigidBody.velocity = Vector2.up * flapStrenght;
+            flapSFX.Play();
+            //birdShowcase.SetActive(false);
+        }
+
+        //if (transform.position.y > 5)
+        //{
+        //    logic.gameOver();
+        //    birdIsAlive = false;
+        //    hitSFX.Play();
+        //    Debug.Log("Game Stopped");
+        //}
+        
+    }
+
+    //to make bird tilting
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Euler(0, 0, birdRigidBody.velocity.y * tiltingSpeed);
+    }
+
+    //trigger game over screen after collide with pipe
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //fade.fadeScreen();
+        logic.gameOver();
+        birdIsAlive = false;
+        hitSFX.Play();
+        Debug.Log("Game Stopped");
+    }
+}
