d18bafa099d0ba307c95dc84d42d32a14b9f9f0e
handanGit <handanmail18@gmail.com>
2023-10-24 08:31:37 +0700

    finishing touch
    final version of v1

--- Assets/MM_scripts.cs
+++ Assets/MM_scripts.cs
@@ -15,6 +15,9 @@ public class MM_scripts : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene("SampleScene");
+        }
     }
 }
